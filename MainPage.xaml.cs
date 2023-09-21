4c015d2f9a906a807c9a4f96f5d34dcf5ca11b81
sebastien <sebastien@robitaille.info>
2023-09-21 10:54:56 -0700

    Added basic calculator functionality

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,131 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private string doOperation(string firstNumber, string secondNumber, char operation)
 	{
-		count++;
+		double result = double.NaN;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+		double firstNumberDouble;
+        double secondNumberDouble;
+
+        if (firstNumber == "")
+		{
+			firstNumberDouble = 0;
+		}
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+		{
+			double.TryParse(firstNumber, out firstNumberDouble);
+		}
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		if(secondNumber == "")
+		{
+			secondNumberDouble = 0;
+		}
+		else
+		{
+            double.TryParse(secondNumber, out secondNumberDouble);
+        }
+        
+        switch (operation)
+		{
+			case '+':
+				result = firstNumberDouble + secondNumberDouble;
+				break;
+			case '-':
+				result = firstNumberDouble - secondNumberDouble;
+				break;
+			case '*':
+				result = firstNumberDouble * secondNumberDouble;
+				break;
+			case '/':
+				result = firstNumberDouble / secondNumberDouble;
+				break;
+		}
+		
+		return $"{firstNumberDouble} {operation} {secondNumberDouble} = {result}";
 	}
+
+	private bool checkIfInputIsValid(string input)
+	{
+		double output;
+		if(input == "")
+		{
+			return true;
+		}
+		else
+		{
+            return double.TryParse(input, out output);
+        }
+		
+	}
+
+	private void toggleAllButtonEnabled(bool isEnabled)
+	{
+		addButton.IsEnabled = isEnabled;
+        subtractButton.IsEnabled = isEnabled;
+        multiplyButton.IsEnabled = isEnabled;
+        divideButton.IsEnabled = isEnabled;
+    }
+
+    void addButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+
+		resultLabel.Text = doOperation(firstNumberEntry.Text, secondNumberEntry.Text, '+');
+    }
+
+    void subtractButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        resultLabel.Text = doOperation(firstNumberEntry.Text, secondNumberEntry.Text, '-');
+    }
+
+    void multiplyButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        resultLabel.Text = doOperation(firstNumberEntry.Text, secondNumberEntry.Text, '*');
+    }
+
+    void divideButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        resultLabel.Text = doOperation(firstNumberEntry.Text, secondNumberEntry.Text, '/');
+    }
+
+    void firstNumberEntry_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+		if (!checkIfInputIsValid(firstNumberEntry.Text)){
+			firstNumberEntry.BackgroundColor = Color.FromRgb(255, 0, 0);
+			toggleAllButtonEnabled(false);
+		}
+        else
+        {
+            firstNumberEntry.BackgroundColor = Color.FromRgb(0, 0, 0);
+			toggleAllButtonEnabled(true);
+        }
+        if (!checkIfInputIsValid(firstNumberEntry.Text) || !checkIfInputIsValid(secondNumberEntry.Text))
+        {
+            toggleAllButtonEnabled(false);
+        }
+    }
+
+    void secondNumberEntry_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+        if (!checkIfInputIsValid(secondNumberEntry.Text)){
+            secondNumberEntry.BackgroundColor = Color.FromRgb(255, 0, 0);
+            toggleAllButtonEnabled(false);
+        }
+		else
+		{
+            secondNumberEntry.BackgroundColor = Color.FromRgb(0, 0, 0);
+            toggleAllButtonEnabled(true);
+        }
+		if(!checkIfInputIsValid(firstNumberEntry.Text) || !checkIfInputIsValid(secondNumberEntry.Text))
+		{
+            toggleAllButtonEnabled(false);
+        }
+    }
 }
 
